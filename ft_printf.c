1999c3f4e0e0b7868321a547a628683a7b1bf63d
Re-Nunes <reenunes.and@gmail.com>
2026-07-02 23:21:11 -0300

    Implement ft_findspecifier function

--- ft_printf.c
+++ ft_printf.c
@@ -1,3 +1,8 @@
+static int ft_findspecifier(va_list args, char *c)
+{
+
+}
+
 int	ft_printf(const char *format, ...)
 {
 	va_list args;
@@ -7,5 +12,19 @@ int	ft_printf(const char *format, ...)
 	i = 0;
 	len = 0;
 	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			len += ft_findspecifier(args, format[i])
+		}
+		else
+		{
+			ft_putchar_fd(format[i], 1)
+			len ++;
+		}
+	}
 	va_end(args);
+	return(len);
 }
